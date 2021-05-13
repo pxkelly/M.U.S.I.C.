18846cf3753739f2dbf1c1c8f8480e6459e6929d
Peter Kelly <pxkelly@hamilton.edu>
2021-05-13 18:03:39 -0400

    Added start to Tone

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import Button from '@material-ui/core/Button';
+import * as Tone from 'tone';
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
+
+    // Binds function to page
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // Starts the Tone library for the synth page
+  handleClick() {
+    Tone.start();
   }
 
   render() {
@@ -27,7 +36,7 @@ class HomePage extends Component {
             <p>It was written in Javascript using the React library</p>
           </Row>
           <Row className="welcome">
-            <a target="_blank" href="https://github.com/pxkelly/MUSIC">Click me to go to the GitHub repository for the code</a>
+            <a href="https://github.com/pxkelly/MUSIC">Click me to go to the GitHub repository for the code</a>
           </Row>
           <br />
           <br />
@@ -35,7 +44,7 @@ class HomePage extends Component {
             <p>Otherwise, click this button to get started!</p>
           </Row>
           <Row className="welcome">
-            <Button variant="contained" color="primary" href="#synth">
+            <Button variant="contained" color="primary" href="#synth" onClick={this.handleClick}>
               Launch M.U.S.I.C.
             </Button>
           </Row>
